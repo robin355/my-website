6f235440dca2e1982fbb2aaf354a29f073bb50a7
Md.Robin Mia <robinmahmud358@gmail.com>
2023-08-09 20:16:57 -0700

    responsive project

--- src/Pages/Home/Experience/Experience.js
+++ src/Pages/Home/Experience/Experience.js
@@ -3,26 +3,26 @@ const Experience = () => {
     return (
         <div>
 
-            <div className='grid gap-8 lg:grid-cols-4 md:grid-cols-2 grid-cols-1 lg:mt-[120px] pr-[100px] pl-[100px]'>
-                <div className='text-center'>
-                    <img className=' ml-[100px]' src='https://mediusware.com/assets/image/c-1.svg' alt="" />
+            <div className='grid gap-6 lg:grid-cols-4  justify-items-center md:grid-cols-2 grid-cols-1 mt-[80px] lg:pl-10'>
+                <div>
+                    <img  src='https://mediusware.com/assets/image/c-1.svg' alt="" />
                     <span className='text-xl text-blue-600'>7+</span>
-                    <p className=''>Years of Experience</p>
+                    <p>Years of Experience</p>
                 </div>
-                <div className='text-center'>
-                    <img className=' ml-[100px]' src='https://mediusware.com/assets/image/c-2.svg' alt="" />
+                <div>
+                    <img src='https://mediusware.com/assets/image/c-2.svg' alt="" />
                     <span className='text-xl text-blue-600'>20+</span>
-                    <p className=''>Countries Served</p>
+                    <p >Countries Served</p>
                 </div>
-                <div className='text-center'>
-                    <img className=' ml-[100px]' src='https://mediusware.com/assets/image/c-3.svg' alt="" />
+                <div className=''>
+                    <img src='https://mediusware.com/assets/image/c-3.svg' alt="" />
                     <span className='text-xl text-blue-600'>50+</span>
-                    <p className=''>IT Professionals</p>
+                    <p>IT Professionals</p>
                 </div>
-                <div className='text-center'>
-                    <img className=' ml-[100px]' src='https://mediusware.com/assets/image/c-4.svg' alt="" />
+                <div>
+                    <img  src='https://mediusware.com/assets/image/c-4.svg' alt="" />
                     <span className='text-xl text-blue-600'>700+</span>
-                    <p className=''>Projects Completed</p>
+                    <p>Projects Completed</p>
                 </div>
             </div>
         </div>
--- src/Pages/Share/Navabar/Navbar.js
+++ src/Pages/Share/Navabar/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
       <div className="navbar bg-[#ffffff]  lg:pl-[100px] lg:pr-[100px]">
         <div className="navbar-start">
           <div className="dropdown">
-            <label tabIndex={0} className="lg:hidden">
+            <label tabIndex={0} className="lg:hidden ">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
             <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-200 rounded-box w-52">
@@ -30,7 +30,7 @@ const Navbar = () => {
               <li><Link className=' bg-[#ffffff] hover:underline hover:text-[#0d6efd]' to='/contact'>Contact</Link></li>
             </ul>
           </div>
-          <Link className="">
+          <Link>
             <div className="avatar">
               <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
                 <img src={image} alt='/' />
